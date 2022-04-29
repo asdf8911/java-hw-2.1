78849942654eadc04737ebc9478886a90c0336ff
Ekaterina <vipolsovatel@mail.ru>
2022-04-29 13:26:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 32058;
+        int oneBonusMile = 20;
+
+        int result;
+        result = priceTicket / oneBonusMile;
+
+        System.out.println(result);
+    }
+}
